728f28c11bda279515f3642979bac7b1a4766ff8
Andy Daniel Martinez <mhdaniel24@gmail.com>
2015-06-28 01:09:25 -0400

    Missing classes from database tables added.

---
+++ LibraryManagement/src/Genre.java
@@ -0,0 +1,29 @@
+
+public class Genre {
+	private String genere_id;
+	private String genere_name;
+	
+	public Genre(String genere_id, String genere_name) {
+		this.genere_id = genere_id;
+		this.genere_name = genere_name;
+	}
+
+	public String getGenere_id() {
+		return genere_id;
+	}
+
+	public void setGenere_id(String genere_id) {
+		this.genere_id = genere_id;
+	}
+
+	public String getGenere_name() {
+		return genere_name;
+	}
+
+	public void setGenere_name(String genere_name) {
+		this.genere_name = genere_name;
+	}
+	
+	
+	
+}
---
+++ LibraryManagement/src/Publisher.java
@@ -0,0 +1,49 @@
+
+public class Publisher {
+	private String publisherId;
+	private String publisherName;
+	private String publisherAddress;
+	private String publisherPhone;
+	
+	public Publisher(String publisherId, String publisherName,String publisherAddress, String publisherPhone) {
+		this.publisherId = publisherId;
+		this.publisherName = publisherName;
+		this.publisherAddress = publisherAddress;
+		this.publisherPhone = publisherPhone;
+	}
+
+	public String getPublisherId() {
+		return publisherId;
+	}
+
+	public void setPublisherId(String publisherId) {
+		this.publisherId = publisherId;
+	}
+
+	public String getPublisherName() {
+		return publisherName;
+	}
+
+	public void setPublisherName(String publisherName) {
+		this.publisherName = publisherName;
+	}
+
+	public String getPublisherAddress() {
+		return publisherAddress;
+	}
+
+	public void setPublisherAddress(String publisherAddress) {
+		this.publisherAddress = publisherAddress;
+	}
+
+	public String getPublisherPhone() {
+		return publisherPhone;
+	}
+
+	public void setPublisherPhone(String publisherPhone) {
+		this.publisherPhone = publisherPhone;
+	}
+	
+	
+	
+}
