c9f1d8b4ba6fb32c3ef2b446ad01004de49c4511
Andy Daniel Martinez <mhdaniel24@gmail.com>
2015-07-02 00:24:43 -0400

    Publisher DAO

--- LMSDAO/src/dao/PublisherDAO.java
+++ LMSDAO/src/dao/PublisherDAO.java
@@ -1,5 +1,54 @@
 package dao;
 
-public class PublisherDAO{
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
+import domain.Author;
+import domain.Publisher;
+
+public class PublisherDAO extends BaseDAO{
+	public void create(Publisher publisher) throws Exception {
+		save("insert into tbl_publisher (publisherName, publisherAddress, publisherPhone) values(?,?,?)",
+				new Object[] { publisher.getPublisherName(), publisher.getPublisherAddress(), publisher.getPublisherPhone() });
+	}
+
+	public void update(Publisher publisher) throws Exception {
+		save("update tbl_publisher set publisherName = ?,  publisherAddress = ?, publisherPhone = ? where publisherId = ?",
+				new Object[] { publisher.getPublisherName(), publisher.getPublisherAddress(), publisher.getPublisherPhone(), publisher.getPublisherId() });
+
+	}
+
+	public void delete(Publisher publisher) throws Exception {
+		save("delete from tbl_publisher where publisherId = ?",
+				new Object[] { publisher.getPublisherId() });
+	}
+
+	public List<Publisher> readAll() throws Exception{
+		return (List<Publisher>) read("select * from tbl_publisher", null);
+		
+	}
+
+	public Publisher readOne(int publisherId) throws Exception {
+		List<Publisher> publishers = (List<Publisher>) read("select * from tbl_publisher", new Object[] {publisherId});
+		if(publishers!=null && publishers.size()>0){
+			return publishers.get(0);
+		}
+		return null;
+	}
+
+	@Override
+	public List extractData(ResultSet rs) throws Exception {
+		List<Publisher> publishers =  new ArrayList<Publisher>();
+		
+		while(rs.next()){
+			Publisher p = new Publisher();
+			p.setPublisherId(rs.getInt("publisherId"));
+			p.setPublisherAddress(rs.getString("publisherAddress"));
+			p.setPublisherName(rs.getString("publisherName"));
+			
+			publishers.add(p);
+		}
+		return publishers;
+	}
 }
