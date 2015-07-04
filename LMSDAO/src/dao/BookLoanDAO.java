a1969bba3ef9d865b6942ac4f8fea10acff9af9a
Andy Daniel Martinez <mhdaniel24@gmail.com>
2015-07-03 22:22:18 -0400

    All DAOs are theoretically done. I havent test them yet.

--- LMSDAO/src/dao/BookLoanDAO.java
+++ LMSDAO/src/dao/BookLoanDAO.java
@@ -6,7 +6,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import domain.Author;
+import domain.Book;
 import domain.BookLoan;
+import domain.Borrower;
+import domain.LibraryBranch;
 
 public class BookLoanDAO extends BaseDAO{
 	
@@ -22,16 +25,17 @@ public class BookLoanDAO extends BaseDAO{
 				bookLoan.getBorrower().getCardNo(), bookLoan.getDateOut(), bookLoan.getDueDate() });
 	}
 
-	//--------------------------------------here------------------------------------------------------
-	public void update(Author author) throws Exception {
-		save("update tbl_author set authorName = ? where authorId = ?",
-				new Object[] { author.getAuthorName(), author.getAuthorId() });
-
+	
+	public void update(BookLoan bookLoan) throws Exception {
+		save("update tbl_book_loans set dateOut = ?, dueDate = ?, dateIn = ? where bookId = ? and cardNo = ? and branchId = ?",
+				new Object[] {bookLoan.getDateOut(), bookLoan.getDueDate(), bookLoan.getDateIn(), bookLoan.getBook().getBookId(),
+				bookLoan.getBorrower().getCardNo(), bookLoan.getLibraryBranch().getBranchId()});
 	}
 
-	public void delete(Author author) throws Exception {
-		save("delete from tbl_author where authorId = ?",
-				new Object[] { author.getAuthorId() });
+	//------------------------here----------------------------
+	public void delete(BookLoan bookLoan) throws Exception {
+		save("delete from tbl_book_loans where bookId = ? and branchId = ? and cardNo = ?",
+				new Object[] { bookLoan.getBook().getBookId(), bookLoan.getLibraryBranch().getBranchId(), bookLoan.getBorrower().getCardNo() });
 	}
 
 	public List<BookLoan> readAll() throws Exception{
@@ -39,31 +43,61 @@ public class BookLoanDAO extends BaseDAO{
 		
 	}
 
-	public Author readOne(int authorId) throws Exception {
-		List<Author> authors = (List<Author>) read("select * from tbl_author where authorId = ?", new Object[] {authorId});
-		if(authors!=null && authors.size()>0){
-			return authors.get(0);
+	public BookLoan readOne(int bookId, int cardNo, int branchId) throws Exception {
+		List<BookLoan> bookLoans = (List<BookLoan>) read("select * from tbl_book_loans where bookId = ? and branchId = ?"
+				+ " and cardNo = ?", new Object[] {bookId, branchId, cardNo});
+		if(bookLoans!=null && bookLoans.size()>0){
+			return bookLoans.get(0);
 		}
 		return null;
 	}
 
 	@Override
-	public List extractData(ResultSet rs) throws Exception {
-		List<Author> authors =  new ArrayList<Author>();
+	public List<BookLoan> extractData(ResultSet rs) throws Exception {
+		List<BookLoan> bookLoans =  new ArrayList<BookLoan>();
+		BookDAO bookdao = new BookDAO(getConnection());
+		LibraryBranchDAO lbranchdao = new LibraryBranchDAO(getConnection());
+		BorrowerDAO borrowerdao = new BorrowerDAO(getConnection());
 		
 		while(rs.next()){
-			Author a = new Author();
-			a.setAuthorId(rs.getInt("authorId"));
-			a.setAuthorName(rs.getString("authorName"));
+			BookLoan bl = new BookLoan();
+			bl.setDateIn(rs.getDate("dateIn"));
+			bl.setDateOut(rs.getDate("dateOut"));
+			bl.setDueDate(rs.getDate("dueDate"));
 			
-			authors.add(a);
+			bl.setBook(bookdao.readOne(rs.getInt("bookId")));
+			bl.setBorrower(borrowerdao.readOne(rs.getInt("cardNo")));
+			bl.setLibraryBranch(lbranchdao.readOne(rs.getInt("cardNo")));
+			
+			bookLoans.add(bl);
 		}
-		return authors;
+		return bookLoans;
 	}
 
 	@Override
-	public List extractDataFirstLevel(ResultSet rs) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+	public List<BookLoan> extractDataFirstLevel(ResultSet rs) throws Exception {
+		List<BookLoan> bookLoans =  new ArrayList<BookLoan>();
+		
+		while(rs.next()){
+			BookLoan bl = new BookLoan();
+			bl.setDateIn(rs.getDate("dateIn"));
+			bl.setDateOut(rs.getDate("dateOut"));
+			bl.setDueDate(rs.getDate("dueDate"));
+			
+//			Book b = new Book();
+//			b.setBookId(rs.getInt("bookId"));
+//			bl.setBook(b);
+//			
+//			LibraryBranch lb = new LibraryBranch();
+//			lb.setBranchId(rs.getInt("branchId"));
+//			bl.setLibraryBranch(lb);
+//			
+//			Borrower bo = new Borrower();
+//			bo.setCardNo(rs.getInt("cardNo"));
+//			bl.setBorrower(bo);
+			
+			bookLoans.add(bl);
+		}
+		return bookLoans;
 	}
 }
